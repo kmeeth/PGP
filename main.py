c8bda3cdfb913c1b4e700c0105b21c545e261d33
kmeeth <matijakup@gmail.com>
2024-06-10 16:21:36 +0200

    Revert "Added rsa_frame."
    This reverts commit ecbe8b1cad8fe6b5e98e8b7999824ed28465267f.

--- main.py
+++ main.py
@@ -1,21 +1,2 @@
-import tkinter as tk
-from tkinter import ttk
-from rsa_frame import rsa_frame
-
-class App(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title("PGP")
-        self.geometry("600x600")
-
-        notebook = ttk.Notebook(self)
-        notebook.pack(expand=True, fill="both")
-
-        frames = [rsa_frame(notebook)]
-        for frame in frames:
-            notebook.add(frame, text=frame.title)
-
-
 if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+    print("PGP")
--- rsa_frame.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-from tkinter import ttk
-def rsa_frame(notebook):
-    frame = ttk.Frame(notebook)
-    frame.title = "RSA"
-    return frame
