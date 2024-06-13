a653bad8cd55ffa12d7c9f10488c7bc830425cfe
kmeeth <matijakup@gmail.com>
2024-06-14 00:25:37 +0200

    Finished encryption, except base64.

--- api.py
+++ api.py
@@ -1,6 +1,11 @@
+import os
 import zlib
 from datetime import datetime
+
+from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.padding import PKCS7
+
 import models
 from models import *
 
@@ -36,8 +41,8 @@ def update_private_ring(user):
     return ring
 
 
-def send_message(message, sender_key_pair, recipient_public_key, need_signature, need_confidentiality, encryption_algorithm):
-    X = bytes(f"{str(datetime.now())}\nFrom: {sender_key_pair.id()}\n{message}", 'utf-8')
+def send_message(message, sender_key_pair, recipient_public_key, need_signature, encryption_algorithm, filename):
+    X = bytes(f"{str(datetime.now())}\nFrom: {sender_key_pair.id()}\nTo: {recipient_public_key.id()}\n{message}", 'utf-8')
     separator = b"||||||||"
     hasher = hashes.Hash(hashes.SHA1())
     hasher.update(X)
@@ -52,8 +57,28 @@ def send_message(message, sender_key_pair, recipient_public_key, need_signature,
 
     X = zlib.compress(X)
 
-    if need_confidentiality:
+    if encryption_algorithm != None:
         session_key = os.urandom(16)
-        
-    print(X)
+        cipher = Cipher(algorithms.CAST5(session_key) if encryption_algorithm == 'CAST5' else algorithms.AES128(session_key), modes.CFB(os.urandom(8 if encryption_algorithm == 'CAST5' else 16)))
+        encryptor = cipher.encryptor()
+        padder = PKCS7(16).padder()
+        padded_data = padder.update(message.encode()) + padder.finalize()
+        encrypted_message = encryptor.update(padded_data) + encryptor.finalize()
+
+        encrypted_key = recipient_public_key.public_key.encrypt(
+            session_key,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA1()),
+                algorithm=hashes.SHA1(),
+                label=None
+            )
+        )
+        X = encrypted_message + separator + encrypted_key + separator + bytes(encryption_algorithm, 'utf-8') + separator + bytes(str(sender_key_pair.id()), 'utf-8')
+
+    # R64 here
+
+    os.makedirs("messages", exist_ok=True)
+    with open(f"messages/{filename}", 'wb') as file:
+        file.write(X)
+    return X
 
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     my_key_pair.save("dobrojutro")
     other_key_pair = models.KeyPair.generate("rahel", 1024)
     other_key_pair.save("dobrojutro")
-    imported_key = models.ImportedKey("rahel", other_key_pair.public_key)
-    imported_key.save("kmeeth")
+    models.ImportedKey("rahel", other_key_pair.public_key).save("kmeeth")
+    models.ImportedKey("kmeeth", my_key_pair.public_key).save("rahel")
     api.import_rings["kmeeth"] = api.update_import_ring("kmeeth")
     api.private_rings["kmeeth"] = api.update_private_ring("kmeeth")
     api.import_rings["rahel"] = api.update_import_ring("rahel")
@@ -17,4 +17,5 @@ if __name__ == "__main__":
     print(api.private_rings["kmeeth"][0])
     print(api.import_rings["kmeeth"][0])
 
-    api.send_message("Hello world", loaded_key, api.import_rings["kmeeth"][0], True, False, None)
+    enc = api.send_message("Hello world", loaded_key, api.import_rings["kmeeth"][0], True, 'CAST5', "msg.txt")
+    print(enc)
